151aeda7ece146f022de8c0a91860b3bf7726741
Brayan Gamboa <bsgv2005@gmail.com>
2024-07-26 14:20:55 -0500

    build(config): implemented differents config (eslint, prettier)

---
+++ eslint.config.mjs
@@ -0,0 +1,29 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+
+
+export default [
+  {
+    files: ["**/*.js"],
+    languageOptions:
+    {
+      sourceType: "commonjs"
+    }
+  },
+  {
+    languageOptions: {
+      globals: globals.browser
+    }
+  },
+  pluginJs.configs.recommended,
+  {
+    rules: {
+      "no-console": ["error", { "allow": ["error"] }],
+      "no-trailing-spaces": "warn",
+      "semi": "error",
+      "comma-dangle": "error",
+      "strict": "error",
+      "require-await": "error"
+    }
+  }
+];
